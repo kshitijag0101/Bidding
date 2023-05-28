dadb1f64678026ec41cbf5eaa1ed6771efae6c2b
Kshitij Agarwal <agarwal.kshitij01@gmail.com>
2023-05-28 20:14:48 +0530

    minor fix

--- routes/bidding.js
+++ routes/bidding.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const isAuth = require("../middlewares/is-auth");
 const bidControllers = require("../controllers/bidding");
+const upload = require("../utils/imageupload.util");
 
 const router = express.Router();
 
